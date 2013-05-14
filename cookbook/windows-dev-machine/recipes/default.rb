60705389baf18aa1e00c2a909af9efd6869ab161
Eddie Groves <eddiegroves@gmail.com>
2013-05-14 21:45:13 +1000

    Adding installation of sysinternals utilities

--- cookbook/windows-dev-machine/recipes/default.rb
+++ cookbook/windows-dev-machine/recipes/default.rb
@@ -1,3 +1,14 @@
-windows_path "c:\\home\\bin" do
+# Sysinternals
+url = 'http://download.sysinternals.com/files/SysinternalsSuite.zip'
+dir = 'c:/home/sysinternals'
+
+windows_zipfile dir do
+  source url
+  action :unzip
+  not_if {::File.exists?("#{dir}/PsExec.exe")}
+end
+
+windows_path dir do
     action :add
 end
+
