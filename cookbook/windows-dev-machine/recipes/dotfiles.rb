e9b0bd68b54fcd9ef621e95cf62bda1519ec235f
Eddie Groves <eddiegroves@gmail.com>
2013-05-19 16:17:12 +1000

    Fixes an issue where plink doesn't exit with code 0

--- cookbook/windows-dev-machine/recipes/dotfiles.rb
+++ cookbook/windows-dev-machine/recipes/dotfiles.rb
@@ -4,8 +4,9 @@ url = node['dotfiles']['url']
 windows_batch 'trust github' do
     code <<-EOH
     echo y | plink git@github.com
+    exit /b 0
     EOH
-    not_if { File.directory?(install_path) }
+    not_if { File.exists?(File.join(ENV['HOME'], '.ssh/known_hosts')) }
 end
 
 execute 'download dotfiles' do
