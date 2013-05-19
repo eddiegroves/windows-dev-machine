c62ba7783c718474f85d3238f5230b5aea7d1240
Eddie Groves <eddiegroves@gmail.com>
2013-05-19 13:18:25 +1000

    Adding ruby devkit for native extension support

--- cookbook/windows-dev-machine/attributes/rubyinstaller.rb
+++ cookbook/windows-dev-machine/attributes/rubyinstaller.rb
@@ -3,3 +3,8 @@ default['rubyinstaller']['path'] = File.join(ENV['SYSTEMDRIVE'], 'Ruby193', 'bin
 default['rubyinstaller']['url'] = 'http://rubyforge.org/frs/download.php/76952/rubyinstaller-1.9.3-p429.exe'
 default['rubyinstaller']['checksum'] = '96537a2d45189cc3bbdd850227443630ebaefa75ca9fdd0829953affa4d9fa8c'
 
+default['rubydevkit']['path'] = File.join(ENV['SYSTEMDRIVE'], 'Ruby193', 'devkit')
+default['rubydevkit']['url'] = 'https://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.2-20111229-1559-sfx.exe'
+default['rubydevkit']['checksum'] = '6c3af5487dafda56808baf76edd262b2020b1b25ab86aabf972629f4a6a54491'
+
+
--- cookbook/windows-dev-machine/recipes/rubyinstaller.rb
+++ cookbook/windows-dev-machine/recipes/rubyinstaller.rb
@@ -2,6 +2,9 @@ package = node['rubyinstaller']['package']
 install_path = node['rubyinstaller']['path']
 url = node['rubyinstaller']['url']
 cksum = node['rubyinstaller']['checksum']
+devkit_path = node['rubydevkit']['path'] 
+devkit_url = node['rubydevkit']['url'] 
+devkit_cksum = node['rubydevkit']['checksum'] 
 
 windows_package package do
     action :install
@@ -10,4 +13,31 @@ windows_package package do
 end 
 
 windows_path(install_path) { action :add }
+
+remote_file "#{Chef::Config[:file_cache_path]}/devkit.exe" do
+    source devkit_url
+    checksum devkit_cksum
+    not_if { File.directory?(devkit_path) }
+end
+
+windows_batch 'unzip self extracting archive' do
+    code <<-EOH
+    7z.exe x #{Chef::Config[:file_cache_path]}/devkit.exe -o#{devkit_path}
+    EOH
+    not_if { File.directory?(devkit_path) }
+end
+
+windows_batch 'run devkit init' do
+    code <<-EOH
+    ruby dk.rb init
+    EOH
+    cwd devkit_path
+end
+
+windows_batch 'run devkit install' do
+    code <<-EOH
+    ruby dk.rb install
+    EOH
+    cwd devkit_path
+end
 
