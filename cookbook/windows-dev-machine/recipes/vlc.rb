09229f92a0b242d480ad645bbb039f92b3fc2257
Eddie Groves <eddiegroves@gmail.com>
2013-05-22 07:56:25 +1000

    Adding VLC installer

---
+++ cookbook/windows-dev-machine/recipes/vlc.rb
@@ -0,0 +1,6 @@
+windows_package 'VLC media player 2.0.6' do
+  source 'http://get.videolan.org/vlc/2.0.6/win32/vlc-2.0.6-win32.exe'
+  checksum '7238aa78b6e18f5e87f277481aa9d3ba035d9d2811df9728b5372790dac06361'
+  action :install
+end
+
