0b80547203ae98d4664716b2448857522cc2c95d
Eddie Groves <eddiegroves@gmail.com>
2013-05-24 12:37:47 +1000

    Adding chocolatey installer

---
+++ cookbook/windows-dev-machine/recipes/chocolatey.rb
@@ -0,0 +1,11 @@
+windows_batch 'installing chocolatey' do
+    code  <<-EOH
+    @powershell -NoProfile -ExecutionPolicy Unrestricted -Command "iex ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+    EOH
+    not_if { File.directory?("#{ENV['systemdrive']}/chocolatey/bin") }
+end
+
+windows_path "#{ENV['systemdrive']}/chocolatey/bin" do
+    action :add
+end
+
