3180e6cd366af316af671dee181b1d208baa3cf9
Eddie Groves <eddiegroves@gmail.com>
2013-05-30 12:08:01 +1000

    Adding checksum for fiddler

--- cookbook/windows-dev-machine/recipes/fiddler.rb
+++ cookbook/windows-dev-machine/recipes/fiddler.rb
@@ -1,5 +1,6 @@
 windows_package 'Fiddler' do
   source 'http://fiddler2.com/docs/default-source/public-downloads/fiddler4setup.exe?sfvrsn=16'
+  checksum 'fff6093496c6e5ff8f9fe18e8f9408510487f5b788370373244de8a5b3764bd8'
   action :install
 end
 
