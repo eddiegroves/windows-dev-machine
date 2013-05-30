debd582ff963cd93dd44bd855cbc784acf8ecce9
Eddie Groves <eddiegroves@gmail.com>
2013-05-30 13:07:18 +1000

    Allowing xunit installer to be cached

--- cookbook/windows-dev-machine/recipes/xunit.rb
+++ cookbook/windows-dev-machine/recipes/xunit.rb
@@ -1,7 +1,11 @@
-windows_zipfile Path.bin('xunit') do
-  action :unzip
+remote_file "#{Chef::Config[:file_cache_path]}/xunit.zip" do
   source 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=xunit&DownloadId=423827&FileTime=129859153262930000&Build=20527'
   checksum 'f76bd2a013606c6c7383ada3e93fce95881bcc033e34754bb391e95a2b5d0fc4'
+end
+
+windows_zipfile Path.bin('xunit') do
+  action :unzip
+  source "#{Chef::Config[:file_cache_path]}/xunit.zip"
   not_if {::File.directory?(Path.bin('xunit')) }
 end
 
