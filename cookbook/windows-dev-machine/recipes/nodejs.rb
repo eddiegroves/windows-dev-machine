bfa71272d47014621c6ae110001cb891e5749d67
Eddie Groves <eddiegroves@gmail.com>
2013-05-30 12:06:09 +1000

    Adding nodejs installer

---
+++ cookbook/windows-dev-machine/recipes/nodejs.rb
@@ -0,0 +1,6 @@
+windows_package 'Node.js' do
+    action :install
+    source 'http://nodejs.org/dist/v0.10.8/x64/node-v0.10.8-x64.msi'
+    checksum '43747df5ce0411b2dd636d572904a3eafbb485e2769efc90eea4a9d6fd1b2d99'
+end
+
