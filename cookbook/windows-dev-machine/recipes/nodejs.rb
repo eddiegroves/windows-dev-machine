019ad43cce54ca08a2f1eff7c510fd6bfecba6c2
Eddie Groves <eddiegroves@gmail.com>
2013-06-17 12:07:04 +1000

    Updating nodejs to 0.10.11

--- cookbook/windows-dev-machine/recipes/nodejs.rb
+++ cookbook/windows-dev-machine/recipes/nodejs.rb
@@ -1,7 +1,7 @@
 windows_package 'Node.js' do
-    source 'http://nodejs.org/dist/v0.10.5/x64/node-v0.10.5-x64.msi'
-    checksum '43747df5ce0411b2dd636d572904a3eafbb485e2769efc90eea4a9d6fd1b2d99'
-    version '0.10.5'
+    source 'http://nodejs.org/dist/v0.10.11/x64/node-v0.10.11-x64.msi'
+    checksum '2145440301f02d008e2f1f38a81869f27a236c4275b90da2c4a940d882090545'
+    version '0.10.11'
     action :install
 end
 
