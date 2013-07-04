9e3192fd4d09992aa57c1592a790c36107e0e9fa
Eddie Groves <eddiegroves@gmail.com>
2013-07-04 11:02:21 +1000

    Fixing xunit download, closes #2

--- cookbook/windows-dev-machine/recipes/xunit.rb
+++ cookbook/windows-dev-machine/recipes/xunit.rb
@@ -1,5 +1,5 @@
 remote_file "#{Chef::Config[:file_cache_path]}/xunit.zip" do
-  source 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=xunit&DownloadId=423827&FileTime=129859153262930000&Build=20527'
+  source 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=xunit&DownloadId=423827&FileTime=129859153262930000&Build=20567'
   checksum 'f76bd2a013606c6c7383ada3e93fce95881bcc033e34754bb391e95a2b5d0fc4'
 end
 
