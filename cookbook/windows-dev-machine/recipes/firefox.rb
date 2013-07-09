5cd9e9126d73abd1907c2752f7c010c23b7da6b5
Eddie Groves <eddiegroves@gmail.com>
2013-07-10 09:05:44 +1000

    Updating VLC and Firefox

--- cookbook/windows-dev-machine/recipes/firefox.rb
+++ cookbook/windows-dev-machine/recipes/firefox.rb
@@ -1,10 +1,10 @@
 # windows_package does not like spaces in the source
 remote_file "#{Chef::Config[:file_cache_path]}/firefox.exe" do
-    source URI.encode("http://releases.mozilla.org/pub/mozilla.org/firefox/releases/21.0/win32/en-US/Firefox Setup 21.0.exe")
-    checksum '61b8710a01d0de6c53b1e60a9e6a455becef0aae86a2589b5bf57140a3a10f9e'
+    source URI.encode("http://releases.mozilla.org/pub/mozilla.org/firefox/releases/22.0/win32/en-US/Firefox Setup 22.0.exe")
+    checksum 'abb189f17e65e73d61843247ad1af7c997318b7854fa850a04e5f9d9a20cdd03'
 end
 
-windows_package 'Mozilla Firefox 21.0 (x86 en-US)' do
+windows_package 'Mozilla Firefox 22.0 (x86 en-US)' do
     source "#{Chef::Config[:file_cache_path]}/firefox.exe"
     installer_type :custom
     options '-ms'
--- cookbook/windows-dev-machine/recipes/vlc.rb
+++ cookbook/windows-dev-machine/recipes/vlc.rb
@@ -1,6 +1,8 @@
-windows_package 'VLC media player 2.0.6' do
-  source 'http://get.videolan.org/vlc/2.0.6/win32/vlc-2.0.6-win32.exe'
-  checksum '7238aa78b6e18f5e87f277481aa9d3ba035d9d2811df9728b5372790dac06361'
+windows_package 'VLC media player 2.0.7' do
+  source 'http://get.videolan.org/vlc/2.0.7/win32/vlc-2.0.7-win32.exe'
+  checksum '08d55bec78e1328dd9f31f01d5d022c026bd8439fd2bf00d95cb4a409eb85747'
+  installer_type :custom
+  options '/S'
   action :install
 end
 
